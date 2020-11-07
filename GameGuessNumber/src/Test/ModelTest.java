b21ae2a2856a84cc4d868094a41a4407b63c6a19
Lena Syrotkina <es.moname@gmail.com>
2020-11-07 10:07:15 +0200

    new test added

--- GameGuessNumber/src/Test/ModelTest.java
+++ GameGuessNumber/src/Test/ModelTest.java
@@ -1,16 +1,21 @@
 package Test;
 
 import com.example.test.Model;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void checkValue() {
         int rightValue = 1;
         Model model = new Model();
         boolean checkValue = (rightValue == 1);
         assertTrue(model.checkValue(1) == checkValue);
+    }
+
+    @Test
+    void testCheckValue() {
     }
 }
